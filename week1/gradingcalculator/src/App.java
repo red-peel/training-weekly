aa61a7a1e4a8ec944a02126b5440f94173f852a8
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-11-15 15:48:47 -0600

    beginning

---
+++ week1/gradingcalculator/src/App.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        boolean run = true;
+        while (run) {
+            System.out.println("Please select an option.");
+            System.out.println("1 for GPA, 2");
+            //calculate()
+        }
+    }
+    // public String calculate(int grade){
+    //     if
+    // }
+}
