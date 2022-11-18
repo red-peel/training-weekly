aa5943579f765573329201b7e10db7260c724f11
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-11-18 11:53:28 -0600

    FIXED JAVALIN INIT

--- week1/demos/hello_server/src/main/java/com/example/App.java
+++ week1/demos/hello_server/src/main/java/com/example/App.java
@@ -1,5 +1,7 @@
 package com.example;
-import 
+
+import io.javalin.Javalin;
+
 /**
  * Hello world!
  */
@@ -15,7 +17,10 @@ public final class App {
         System.out.println("Hello World!");
         int port = (7000);
         Javalin app = Javalin.create().start(port);
-        System.out.println("Port created at "+port);
-        app.get("/", ctx -> ctx.result(""));
+        app.get("/", ctx -> ctx.result("Hi dude"));
+        app.get("/hello", ctx -> {
+            ctx.result("<Hi dude, ADVANCED>");
+            ctx.status(418);
+        });
     }
 }
